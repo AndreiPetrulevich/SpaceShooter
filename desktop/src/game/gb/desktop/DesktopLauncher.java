c8f4ead76fe8ca432a4ebd8251dc5995e775cbf6
Ond Re <aapetrulevich@icloud.com>
2021-05-23 17:52:48 +0300

    Initial commit

---
+++ desktop/src/game/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.gb.LittleNinja;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LittleNinja(), config);
+	}
+}
