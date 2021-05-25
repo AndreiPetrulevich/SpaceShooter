5f9846f14ec96fe09e624853aad7d253acf87ce9
Ond Re <aapetrulevich@icloud.com>
2021-05-26 01:43:19 +0300

    HomeTask2: move ball to position

--- core/src/game/gb/screen/MenuScreen.java
+++ core/src/game/gb/screen/MenuScreen.java
@@ -7,26 +7,44 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import game.gb.base.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
-
+    private final int ballSize = 100;
+    private final int velocity = 200;
     private Texture img;
     private Vector2 position;
-    private Vector2 vMove;
+    private Vector2 targetPosition;
+    private Vector2 moveOffset;
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("testBackground.jpg");
+        img = new Texture("ball.png");
         position = new Vector2();
-        vMove = new Vector2(1,0);
+        targetPosition = new Vector2();
+        moveOffset = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        position.add(vMove);
-        ScreenUtils.clear(1, 0, 0, 1);
+
+        if (!targetPosition.epsilonEquals(position)) {
+            Vector2 newPosition = position.cpy().mulAdd(moveOffset, delta * velocity);
+            if (position.x < newPosition.x) {
+                newPosition.x = Math.min(newPosition.x, targetPosition.x);
+            } else {
+                newPosition.x = Math.max(newPosition.x, targetPosition.x);
+            }
+            if (position.y < newPosition.y) {
+                newPosition.y = Math.min(newPosition.y, targetPosition.y);
+            } else {
+                newPosition.y = Math.max(newPosition.y, targetPosition.y);
+            }
+            position = newPosition;
+        }
+
+        ScreenUtils.clear(1, 1, 1, 1);
         batch.begin();
-        batch.draw(img, position.x, position.y);
+        batch.draw(img, position.x, position.y, ballSize, ballSize);
         batch.end();
     }
 
@@ -38,7 +56,8 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        position.set(screenX, Gdx.graphics.getHeight() - screenY);
+        targetPosition.set(screenX - ballSize / 2, Gdx.graphics.getHeight() - screenY - ballSize / 2);
+        moveOffset = targetPosition.cpy().sub(position).nor();
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
